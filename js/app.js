d3cf45eab3fde96a284a71552dd179072c699ab1
Victor <viclee02@gmail.com>
2016-09-23 17:34:00 -1000

    refactor and add new exercise

--- js/app.js
+++ js/app.js
@@ -1,50 +1,61 @@
-
-  /*1.  Fill in the correct id to target the paragraph tag*/
+/*Exercise 1.  Fill in the correct id to target the paragraph tag*/
   function fontSize(){
-    document.getElementById().style.fontSize = "200%";
+    document.getElementById(______).style.fontSize = "200%";
   }
 
- /* 2. change the font color to red*/
+ /*Exercise 2. change the font color to red*/
   function colorMeBadd(){
-    document.getElementById().style.color = ''; 
+    document.getElementById(_______).style.color = '______'; 
   }
 
-/*3. fill in the spaces to make this function operate:*/
+/*Exercise 3. fill in the spaces to make this function operate:*/
   function showAnswer(){
-    document.getElementById().innerHTML = " C (da sea get it?) arrrr!";
+    document.getElementById(_______).________ = " C (da sea get it?) arrrr!";
   }
 
-  /*4.  Create a function that will display the message "My name is Lee, Brrruuuce Lee!" in a new paragraph element after the button is clicked.*/
+/*Exercise 4.  Instruct the following function to display the message "My name is Lee, Brrruuuce Lee!" in a new paragraph element after the button is clicked.*/
+
+  function kungfu(){
+    ________.getElementById(________).________ = "My name is Lee, Brrruuuce Lee!";
+  }
 
 
 //Event Listeners
 //Please write your code inside the window.onload function for exerices 5 onward...
 
-window.onload = function(){
+window.onload = function (){
+/*Exercise 5. Add an event listener to the quotes button that will display a quote after it is clicked on.</p>*/
 
-document.getElementById().addEventListener('event', sayQuote);
+var firstQuote = "Our lives are defined by opportunities; even the ones we miss.";
+
+_________.getElementById(________).addEventListener(______, sayQuote);
 
 function sayQuote(){
-  document.getElementById().innerHTML = "Our lives are defined by opportunities; even the ones we miss.";
+  document._________(_______).__________ = __________;
 }
 
 
-//6.  Generate a random quote from clicking on a button
-var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance."];
+/*Exercise 6. Add another event listener to the image below that will display an inspirational quote after it is clicked</p>*/
 
-  document.getElementById().addEventListener("click", sayQuotes);
 
- 
+
+
+/*Exercise 7.  Generate a random quote from clicking on a button*/
+var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
+
+
+                                                   
+/*Exercise 8. Create an event listener that will show and hide the message when clicking on the button.*/
+
 
 
 /*Final Boss:
 Make a visual list of the items in the orders array. When clicking on the product, you must show and hide the price for that item.*/
 
 
-var orders = [{product: "Space Cat T-shirt", price: 100}, {product: "Barry Manilow's Greatest Hits Colletion Vol 1", price: 500}, {product: "Chewbacca Mask", price: 3.41}, {product: "Woof Washer 360", price: 9.99}, {product: "Sauna Pants", price: 39.95}];
+var orders = [{product: "Finger Toothbrush", price: 9.99}, {product: "Barry Manilow's Greatest Hits Collection Vol 1", price: 500.01}, {product: "Goldfish Aquairum Phone Booth", price: 199.99}, {product: "Woof Washer 360", price: 9.99}, {product: "Sauna Pants", price: 39.95}, {product: "Noiseless Mute Karaoke Microphone", price: 79.99}, {product: "Necktie Umbrella", price: 16.86}];
 
 
-  
 };
 
 
